9ba4370aad9d798334aa3180e12aa2d638dcfb1d
SamuelOkwalinga <sookwalinga@gmail.com>
2021-06-21 10:24:06 +0300

    Dynamically loop through the team and technology and render in a list

--- client/src/components/views/DetailProjectPage/Sections/ProjectInfo.js
+++ client/src/components/views/DetailProjectPage/Sections/ProjectInfo.js
@@ -9,7 +9,24 @@ function ProjectInfo(props) {
     setProject(props.detail);
   }, [props.detail]);
 
-  return (
+  // Loop through technologies and display in a list.
+  const technology = [1, 2, 3, 4, 5];
+  // Project.technology;
+
+  const listTechnologies = technology.map((item) =>
+    <li>{item}</li>
+  );
+
+  // Loop through team and display in a list.
+  const team = [1, 2, 3, 4, 5];
+  // Project.technology;
+
+  const listTeam = team.map((member) =>
+    <li>{member}</li>
+  );
+
+
+    return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
@@ -19,157 +36,91 @@ function ProjectInfo(props) {
             sx={{ pl: 0, fontSize: "1.2rem" }}
             variant="body2"
           >
-            <ArrowRightIcon fontSize="large" />{Project.title}
-            {/* {state} */}
+            <ArrowRightIcon fontSize="large" />
+            {Project.categories}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="textSecondary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ p: 1 }}
+            gutterBottom
+            variant="h5"
+          >
+            {Project.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="tertiary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ pl: 1 }}
+            gutterBottom
+            variant="h6"
+          >
+            {Project.description}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="textSecondary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ p: 1 }}
+            gutterBottom
+            variant="h5"
+          >
+            Technology
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="tertiary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ pl: 1 }}
+            gutterBottom
+            variant="h6"
+          >
+            {listTechnologies}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="textSecondary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ p: 1 }}
+            gutterBottom
+            variant="h5"
+          >
+            Team
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography
+            align="left"
+            color="tertiary"
+            display="inline"
+            margin="0rem 0.5rem"
+            sx={{ pl: 1 }}
+            gutterBottom
+            variant="h6"
+          >
+            {listTeam}
           </Typography>
         </Grid>
       </Grid>
     </Box>
-
-    // <div className="col-6">
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       color="#EA0008"
-    //       display="inline"
-    //       sx={{ pl: 0, fontSize: "1.2rem" }}
-    //       variant="body2"
-    //     >
-    //       <ArrowRightIcon fontSize="large" />
-    //       {Project.categories}
-    //       {/* {state} */}
-    //     </Typography>
-    //   </Grid>
-
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="textSecondary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ p: 1 }}
-    //       gutterBottom
-    //       variant="h5"
-    //     >
-    //       {Project.title}
-    //     </Typography>
-    //   </Grid>
-
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="tertiary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ pl: 1 }}
-    //       gutterBottom
-    //       variant="h6"
-    //     >
-    //       {Project.description}
-    //     </Typography>
-    //   </Grid>
-
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="textSecondary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ p: 1 }}
-    //       gutterBottom
-    //       variant="h5"
-    //     >
-    //       Technology
-    //     </Typography>
-    //   </Grid>
-
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="tertiary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ pl: 1 }}
-    //       gutterBottom
-    //       variant="h6"
-    //     >
-    //       <ul>
-    //         <li>{Project.technology}</li>
-    //       </ul>
-    //     </Typography>
-    //   </Grid>
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="textSecondary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ p: 1 }}
-    //       gutterBottom
-    //       variant="h5"
-    //     >
-    //       Team
-    //     </Typography>
-    //   </Grid>
-
-    //   <Grid
-    //     item
-    //     sx={{
-    //       alignItems: "center",
-    //       display: "flex",
-    //     }}
-    //   >
-    //     <Typography
-    //       align="left"
-    //       color="tertiary"
-    //       display="inline"
-    //       margin="0rem 0.5rem"
-    //       sx={{ pl: 1 }}
-    //       gutterBottom
-    //       variant="h6"
-    //     >
-    //       <ul>
-    //         <li>{Project.team}</li>
-    //       </ul>
-    //     </Typography>
-    //   </Grid>
-    // </div>
-    // </div>
   );
 }
 
