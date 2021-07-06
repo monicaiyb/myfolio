f90b59ae8edd32c535ca1b922512773452d063c3
SamuelOkwalinga <sookwalinga@gmail.com>
2021-07-06 13:07:53 +0300

    Add admin dashboard

---
+++ client/src/components/views/admin/Articles.js
@@ -0,0 +1,229 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import FilledInput from "@material-ui/core/FilledInput";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormLabel from "@material-ui/core/FormLabel";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+// @material-ui/icons components
+
+// core components
+import ArticleHeader from "components/Headers/ArticleHeader.js";
+
+import componentStyles from "assets/theme/views/admin/profile.js";
+
+const useStyles = makeStyles(componentStyles);
+
+function Profile() {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <>
+      <ArticleHeader />
+      {/* Page content */}
+      <Container
+        maxWidth={false}
+        component={Box}
+        marginTop="-6rem"
+        classes={{ root: classes.containerRoot }}
+      >
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            xl={12}
+            component={Box}
+            marginBottom="3rem"
+            classes={{ root: classes.gridItemRoot + " " + classes.order2 }}
+          >
+            <Card
+              classes={{
+                root: classes.cardRoot + " " + classes.cardRootSecondary,
+              }}
+            >
+              <CardHeader
+                subheader={
+                  <Grid
+                    container
+                    component={Box}
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Grid item xs="auto">
+                      <Box
+                        component={Typography}
+                        variant="h3"
+                        marginBottom="0!important"
+                      >
+                        New Article
+                      </Box>
+                    </Grid>
+                  </Grid>
+                }
+                classes={{ root: classes.cardHeaderRoot }}
+              ></CardHeader>
+              <CardContent>
+                <Box
+                  component={Typography}
+                  variant="h6"
+                  color={theme.palette.gray[600] + "!important"}
+                  paddingTop=".25rem"
+                  paddingBottom=".25rem"
+                  fontSize=".75rem!important"
+                  letterSpacing=".04em"
+                  marginBottom="1.5rem!important"
+                  classes={{ root: classes.typographyRootH6 }}
+                >
+                  Posted By
+                </Box>
+                <div className={classes.plLg4}>
+                  <Grid container>
+                    <Grid item xs={12} lg={6}>
+                      <FormGroup>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl
+                          variant="filled"
+                          component={Box}
+                          width="100%"
+                          marginBottom="1rem!important"
+                        >
+                          <Box
+                            paddingLeft="0.75rem"
+                            paddingRight="0.75rem"
+                            component={FilledInput}
+                            autoComplete="off"
+                            type="text"
+                            defaultValue="Samuel Teko"
+                            placeholder="Samuel Teko"
+                          />
+                        </FormControl>
+                      </FormGroup>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                      <FormGroup>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl
+                          variant="filled"
+                          component={Box}
+                          width="100%"
+                          marginBottom="1rem!important"
+                        >
+                          <Box
+                            paddingLeft="0.75rem"
+                            paddingRight="0.75rem"
+                            component={FilledInput}
+                            autoComplete="off"
+                            type="email"
+                            placeholder="teko@gmc.com"
+                          />
+                        </FormControl>
+                      </FormGroup>
+                    </Grid>
+                  </Grid>
+                </div>
+                <Box
+                  component={Divider}
+                  marginBottom="1.5rem!important"
+                  marginTop="1.5rem!important"
+                />
+                <Box
+                  component={Typography}
+                  variant="h6"
+                  color={theme.palette.gray[600] + "!important"}
+                  paddingTop=".25rem"
+                  paddingBottom=".25rem"
+                  fontSize=".75rem!important"
+                  letterSpacing=".04em"
+                  marginBottom="1.5rem!important"
+                  classes={{ root: classes.typographyRootH6 }}
+                >
+                  Article Content
+                </Box>
+                <div className={classes.plLg4}>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <FormGroup>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl
+                          variant="filled"
+                          component={Box}
+                          width="100%"
+                          marginBottom="1rem!important"
+                        >
+                          <Box
+                            paddingLeft="0.75rem"
+                            paddingRight="0.75rem"
+                            component={FilledInput}
+                            autoComplete="off"
+                            type="text"
+                            defaultValue=""
+                          />
+                        </FormControl>
+                      </FormGroup>
+                    </Grid>
+                  </Grid>
+                </div>
+                <div className={classes.plLg4}>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <FormGroup>
+                        <FormLabel>Content</FormLabel>
+                        <FormControl
+                          variant="filled"
+                          component={Box}
+                          width="100%"
+                          marginBottom="1rem!important"
+                        >
+                          <Box
+                            paddingLeft="0.75rem"
+                            paddingRight="0.75rem"
+                            component={FilledInput}
+                            autoComplete="off"
+                            multiline
+                            defaultValue="Enter content here..."
+                            rows="4"
+                          />
+                        </FormControl>
+                      </FormGroup>
+                    </Grid>
+                  </Grid>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        <Container
+              display="flex"
+              alignItems="center"
+              maxWidth={false}
+              component={Box}
+              classes={{ root: classes.containerRoot }}
+              marginTop="1.5rem!important"
+            >
+              <Grid container
+                alignItems="center"
+              >
+                <Button
+                  variant="contained"
+                  classes={{ root: classes.buttonRoot }}
+                >
+                  Post Article
+                </Button>
+              </Grid>
+            </Container>
+      </Container>
+    </>
+  );
+}
+
+export default Profile;
